db75ae165fa2f0ec599963a940e1f6f843e3766e
6q3e <kumakumakumamakuma@gmail.com>
2026-01-31 22:32:26 +0900

    update inital board exec

--- main.py
+++ main.py
@@ -30,6 +30,18 @@ def is_valid(board, r, c, num):
                 return False
     return True
 
+def check_initial_validity(board):
+    for r in range(9):
+        for c in range(9):
+            num = board[r][c]
+            if num != 0:
+                board[r][c] = 0
+                if not is_valid(board, r, c, num):
+                    board[r][c] = num
+                    return False
+                board[r][c] = num
+    return True
+
 def solve(board):
     for r in range(9):
         for c in range(9):
@@ -45,9 +57,17 @@ def solve(board):
 
 def solve_sudoku(event):
     message_el = document.querySelector("#message")
-    message_el.innerText = "計算中"
+    message_el.style.color = "var(--text-color)"
+    message_el.innerText = "チェック中"
 
     board = get_board()
+
+    if not check_initial_validity(board):
+        message_el.innerText = "エラー：同じ列・行・ブロック内で数字が重複しています"
+        message_el.style.color = "#d63384"
+        return
+
+    message_el.innerText = "計算中"
 
     initial_indices = []
     for r in range(9):
